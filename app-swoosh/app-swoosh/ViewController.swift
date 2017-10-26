97231d6cd34dac1c222e2f91aea2848e186270a8
Alex LaDue <ajladue@MacBook-Pro-441.fios-router.home>
2017-10-25 23:26:45 -0400

    Lesson 2

--- app-swoosh/app-swoosh/ViewController.swift
+++ app-swoosh/app-swoosh/ViewController.swift
@@ -10,9 +10,16 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var swoosh: UIImageView!
+    @IBOutlet weak var bgImg: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        //This will put the Swoosh logo right in the middle of the screen regardless of screen size
+        swoosh.frame = CGRect(x: view.frame.size.width/2 - swoosh.frame.size.width/2  , y: 50 , width: swoosh.frame.size.width, height: swoosh.frame.size.height)
+        
+        //Same thing for the bgImg. Can call .position or . size - Not putting either will do both
+        bgImg.frame = view.frame
     }
 
     override func didReceiveMemoryWarning() {
