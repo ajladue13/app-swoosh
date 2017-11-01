bb2fc56bffa660e0bca2c403f445407b5a96419b
Alex LaDue <aj.ladue@gmail.com>
2017-10-31 22:42:44 -0400

    Lesson 7

--- app-swoosh/app-swoosh/Controller/WelcomeVC.swift
+++ app-swoosh/app-swoosh/Controller/WelcomeVC.swift
@@ -1,27 +1,24 @@
 //
-//  ViewController.swift
+//  WelcomeVC.swift
 //  app-swoosh
 //
-//  Created by Alex LaDue on 10/25/17.
+//  Created by Alex LaDue on 10/31/17.
 //  Copyright © 2017 Alex LaDue. All rights reserved.
 //
 
 import UIKit
 
-class ViewController: UIViewController {
+class WelcomeVC: UIViewController {
 
     @IBOutlet weak var swoosh: UIImageView!
     @IBOutlet weak var bgImg: UIImageView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
     }
-
+    
     @IBAction func unwindFromSkillVC(unwindSegue: UIStoryboardSegue) {
         
     }
     
-    
 }
-
